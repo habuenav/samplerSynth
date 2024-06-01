d51a4e36dff7e0fb0f9bb3196eab068877a20544
Holman Buenaventura <habuenav@hotmail.com>
2024-05-31 20:00:32 -0500

    Update rtMidiIn.ino

--- examples/rtMidiIn/rtMidiIn.ino
+++ examples/rtMidiIn/rtMidiIn.ino
@@ -1,30 +1,45 @@
 #include <samplerSynth.h>
 #include <MIDI.h>
 
-struct CustomBaudRate : public MIDI_NAMESPACE::DefaultSettings {
-  static const long BaudRate = 31250;
-};
+struct CustomBaudRate : public MIDI_NAMESPACE::DefaultSettings {  static const long BaudRate = 31250;};
 
-//check Rx pin for Serial1 on your board, Wemos lolin32 lite is pin 26
-MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI,CustomBaudRate); 
+MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI,CustomBaudRate);
+USING_NAMESPACE_MIDI;
+typedef Message<MIDI_NAMESPACE::DefaultSettings::SysExMaxSize> MidiMessage;
 
-void handleNoteOn(byte channel, byte pitch, byte velocity)
-{    notaOn(pitch,velocity,0); 
+bool velocidad=true;
+
+void onMessage(const MidiMessage& message) {
+  byte instr,speed;
+  
+  switch (message.type) 
+   {
+    case NoteOn:  speed=velocidad?message.data2:100; notaOn(message.data1,speed);  break;
+    case NoteOff: notaOff(message.data1);     break;
+    case ProgramChange: instr=(message.data1>12)?12:message.data1; setInstrumento(instr); break;
+    case ControlChange: alterPitchNota(message.data2);   break;
+    case PitchBend:  alterVolNota(message.data2);   break;
+  }
 }
-void handleNoteOff(byte channel, byte pitch, byte velocity)
-{    notaOff(pitch);
-}
-
+void handleStart(){  setSustainNota(0); }
+void handleContinue(){ velocidad=!velocidad; }
+void handleStop(){  setSustainNota(700); }
+// -----------------------------------------------------------------------------
 void setup()
-{initSynth(16,25,22); //bck,ws,data 
-delay(1000);
-setVolumen(2);
-setInstrumento(0);
-MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
-MIDI.setHandleNoteOff(handleNoteOff);
-MIDI.begin(MIDI_CHANNEL_OMNI);
+{
+  initSynth(16,25,22);
+  delay(100);
+  setVolumen(40);
+  setInstrumento(0);
+  Serial.begin(115200);
+   
+   MIDI.setHandleMessage(onMessage);
+   MIDI.setHandleStart(handleStart);
+   MIDI.setHandleContinue(handleContinue);
+   MIDI.setHandleStop(handleStop);
+   MIDI.begin(MIDI_CHANNEL_OMNI);
 }
-
 void loop()
-{   MIDI.read();
+{
+   MIDI.read();
 }
