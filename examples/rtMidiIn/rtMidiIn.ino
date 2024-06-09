f2ae0f9e1052abad9ffae266d6c14b83dbb05c7b
Holman Buenaventura <habuenav@hotmail.com>
2024-06-09 02:35:55 -0500

    Update rtMidiIn.ino
    change midi library

--- examples/rtMidiIn/rtMidiIn.ino
+++ examples/rtMidiIn/rtMidiIn.ino
@@ -1,45 +1,30 @@
 #include <samplerSynth.h>
-#include <MIDI.h>
-
-struct CustomBaudRate : public MIDI_NAMESPACE::DefaultSettings {  static const long BaudRate = 31250;};
-
-MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI,CustomBaudRate);
-USING_NAMESPACE_MIDI;
-typedef Message<MIDI_NAMESPACE::DefaultSettings::SysExMaxSize> MidiMessage;
+#include <midiRead.h>
 
 bool velocidad=true;
-
-void onMessage(const MidiMessage& message) {
-  byte instr,speed;
-  
-  switch (message.type) 
-   {
-    case NoteOn:  speed=velocidad?message.data2:100; notaOn(message.data1,speed);  break;
-    case NoteOff: notaOff(message.data1);     break;
-    case ProgramChange: instr=(message.data1>12)?12:message.data1; setInstrumento(instr); break;
-    case ControlChange: alterPitchNota(message.data2);   break;
-    case PitchBend:  alterVolNota(message.data2);   break;
+void onMidi(MidiMessageTypes type, int channel, int data1, int data2)
+{   byte instr,speed;
+  switch(type){
+    case NoteOn:  speed=velocidad?data2:100; notaOn(data1,speed);  break;
+    case NoteOff: notaOff(data1);     break;
+    case ProgramChange: instr=(data1>12)?12:data1; setInstrumento(instr); break;
+    case ControlChange: alterPitchNota(data2);   break;
+    case PitchBend:  alterVolNota(data2);   break;
   }
 }
-void handleStart(){  setSustainNota(0); }
-void handleContinue(){ velocidad=!velocidad; }
-void handleStop(){  setSustainNota(700); }
-// -----------------------------------------------------------------------------
-void setup()
-{
+MidiRead midi(Serial1,31250);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
   initSynth(16,25,22);
   delay(100);
-  setVolumen(40);
-  setInstrumento(0);
-  Serial.begin(115200);
-   
-   MIDI.setHandleMessage(onMessage);
-   MIDI.setHandleStart(handleStart);
-   MIDI.setHandleContinue(handleContinue);
-   MIDI.setHandleStop(handleStop);
-   MIDI.begin(MIDI_CHANNEL_OMNI);
+  setVolumen(30);
+  setInstrumento(1);
+  midi.handleMidi(onMidi);
 }
-void loop()
-{
-   MIDI.read();
+
+// the loop function runs over and over again forever
+void loop() {
+       midi.read();            // wait for a second
 }
